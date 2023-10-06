4a404d8880777699c8537b7c1c0848e19c57bf72
Hemerson de Carli <hemersondc96@gmail.com>
2023-10-06 15:27:47 +0100

    lab2-done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import Content from './components/content';
+import Footer from './components/footer';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Header></Header>
+            <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Content></Content>}></Route>
+        <Route path='create' element={<Footer></Footer>}></Route>
+        <Route path='read' element={<Header></Header>}></Route>
+        
+      </Routes>
+      {/* <Header></Header>
       <Content></Content>
+      <Footer></Footer> */}
     </div>
+    </BrowserRouter>
   );
 }
 
